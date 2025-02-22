8fcc8fd2475182c334d32c21d4fa779d023cff8b
AmanYadav5072005 <amanaj8873@gmail.com>
2025-02-22 19:21:27 +0530

    22/2/2025

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -5,8 +5,12 @@ function App() {
  
   
   return (
+    <div>
     
     <h1>Welcome to React Classes</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos sunt, consequuntur et reiciendis dignissimos optio similique. Ratione voluptatum temporibus nemo, cum exercitationem dolorem ullam dicta quia velit sed tenetur sequi?</p>
+    <img src="forest-tree-natur-autmun.jpg"  />
+    </div>
   )
    
    
